9368f407e287ae33e5740235af10bb0270955189
Mykyta Nedialkov <mykyta.nedialkov@webpros.com>
2023-05-24 15:52:55 +0200

    Finish app

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useRef} from 'react';
 
 const TodoForm = (props) => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
   const inputRef = useRef(null)
 
@@ -17,27 +17,41 @@ const TodoForm = (props) => {
     e.preventDefault();
 
     props.onSubmit({
-      id: Math.floor(Math.random() * 10000),
-      text: input,
+      id: Math.floor(Math.random() * 10000), text: input,
     });
 
     setInput('');
   }
 
-  return (
-    <form className="todo-form" onSubmit={handleSubmit}>
-      <input
-        type='text'
-        placeholder="Add a todo"
-        value={input}
-        name="text"
-        className="todo-input"
-        onChange={handleChange}
-        ref={inputRef}
-      />
-      <button className="todo-button">Add todo</button>
-    </form>
-  );
+  return (<form className="todo-form" onSubmit={handleSubmit}>
+      {props.edit ? (
+        <>
+          <input
+            type='text'
+            placeholder="Update your item"
+            value={input}
+            name="text"
+            className="todo-input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button edit">Update</button>
+        </>
+      ) : (
+        <>
+          <input
+            type='text'
+            placeholder="Add a todo"
+            value={input}
+            name="text"
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button">Add todo</button>
+        </>
+      )}
+    </form>);
 };
 
 export default TodoForm;
